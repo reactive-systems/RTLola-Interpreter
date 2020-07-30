c37caed213326ac6f87de76361eab6d362161818
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2020-07-30 08:14:28 +0200

    Transform workspace into single crate.

--- afl-fuzz-frontend/fuzz.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "building afl..."
-RUSTFLAGS='-C codegen-units=1' cargo afl build
-cargo afl fuzz -i in -o out target/debug/afl-fuzz-frontend
--- afl-fuzz-frontend/fuzz_clean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./seed.sh
-./fuzz.sh
