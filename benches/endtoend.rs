9544fdc0321de0f39f175f4e710dc250e430dfd7
Malte Schledjewski <malte.schledjewski@cispa.de>
2021-04-09 18:32:57 +0200

    CI: fix relative paths for cargo bench

--- benches/endtoend.rs
+++ benches/endtoend.rs
@@ -10,8 +10,8 @@ fn endtoend_semi_complex_spec(b: &mut Bencher) {
     let config = Config::new(&[
         "rtlola-interpreter".to_string(),
         "monitor".to_string(),
-        "../traces/spec_offline.lola".to_string(),
-        "--csv-in=../traces/timed/trace_0.csv".to_string(),
+        "traces/spec_offline.lola".to_string(),
+        "--csv-in=traces/timed/trace_0.csv".to_string(),
         "--verbosity=silent".to_string(),
         "--offline".to_string(),
     ]);
@@ -25,8 +25,8 @@ fn endtoend_semi_complex_spec_triggers(b: &mut Bencher) {
     let config = Config::new(&[
         "rtlola-interpreter".to_string(),
         "monitor".to_string(),
-        "../traces/spec_offline.lola".to_string(),
-        "--csv-in=../traces/timed/trace_0_small.csv".to_string(),
+        "traces/spec_offline.lola".to_string(),
+        "--csv-in=traces/timed/trace_0_small.csv".to_string(),
         "--verbosity=triggers".to_string(),
         "--time-info-rep=relative".to_string(),
         "--offline".to_string(),
