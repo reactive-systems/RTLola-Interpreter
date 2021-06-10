3fa0db04ee01c7dc2c066b7f3721023f890776a8
Florian Kohn <florian.kohn@cispa.de>
2021-06-10 11:29:02 +0200

    Removed naive evaluation of expressions

--- src/basics/mod.rs
+++ src/basics/mod.rs
@@ -6,9 +6,7 @@ mod pcap_input;
 
 pub type Time = Duration;
 
-pub use self::config::{
-    EvalConfig, EvaluatorChoice, ExecutionMode, Statistics, TimeFormat, TimeRepresentation, Verbosity,
-};
+pub use self::config::{EvalConfig, ExecutionMode, Statistics, TimeFormat, TimeRepresentation, Verbosity};
 pub use self::io_handler::OutputChannel;
 pub(crate) use self::io_handler::{create_event_source, EventSource, EventSourceConfig, OutputHandler};
 
