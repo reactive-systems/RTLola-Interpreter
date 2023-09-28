0d711bcce37bd524be6b74eeea830f86aee352fb
Florian Kohn <florian.kohn@cispa.de>
2023-09-28 14:34:44 +0200

    remove should fail macro compile test due to too much variance.

--- crates/rtlola-interpreter-macros/tests/macro_compile.rs
+++ crates/rtlola-interpreter-macros/tests/macro_compile.rs
@@ -3,12 +3,10 @@ fn derive_value_factory() {
     let t = trybuild::TestCases::new();
     t.pass("examples/simple.rs");
     t.pass("examples/custom_names.rs");
-    t.compile_fail("examples/should-fail/reject_value.rs");
 }
 
 #[test]
 fn derive_composit_factory() {
     let t = trybuild::TestCases::new();
     t.pass("examples/enum.rs");
-    t.compile_fail("examples/should-fail/reject_composit.rs");
 }
